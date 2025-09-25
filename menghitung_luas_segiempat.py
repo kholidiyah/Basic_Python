b85e2ae00f1d6ca1118ce6ba1d088fe26bd92e82
kholidiyahm <150261222+kholidiyah@users.noreply.github.com>
2025-09-25 09:44:48 +0700

    Add files via upload

---
+++ menghitung_luas_segiempat.py
@@ -0,0 +1,14 @@
+### Langkah-Langkah Membuat Program ###
+#1. Mendefinisikan Masalah
+##2. Merumuskan Solusi
+#3. Membuat Program
+#4. Menguji Program
+#5. Membuat Dokumentasi
+#6. Pemrliharaan
+
+#Menghitung Luas Segiempat#
+tinggi = int(input("masukan tinggi:   "))
+panjang = int(input("masukan panjang:   "))
+luas = panjang * tinggi
+print("Luas segi empat:"   , luas)
+ 
